78b2fec36158a9c13a42de521eb7b1ff04670eaa
SHINSEOUNGROK <tnsfhr03@gmail.com>
2023-03-01 22:32:38 +0900

    스프링 빈 조회 - 기본

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -26,8 +26,8 @@ class ApplicationContextInfoTest {
         for (String beanDefinitionName : beanDefinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
 
-            //Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
-            //ROle ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
+            // Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
+            // Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
 
             if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
                 Object bean = ac.getBean(beanDefinitionName);
