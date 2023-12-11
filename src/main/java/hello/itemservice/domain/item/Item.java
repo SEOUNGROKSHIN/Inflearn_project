5f4f30f08c8602a0c2d87ef0ed609a7de5817e50
SHINSEOUNGROK <tnsfhr03@gmail.com>
2023-12-11 20:31:24 +0900

    상품 등록 - POST

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,19 +1,15 @@
 package hello.itemservice.domain.item;
-
 import lombok.Data;
-
 @Data
 public class Item {
 
     private Long id;
     private String itemName;
     private Integer price;
-    private int quantity;
-
-    public Item(Long id) {
+    private Integer quantity;
+    public Item() {
     }
-
-    public Item(String itemName, Integer price, int quantity) {
+    public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
         this.price = price;
         this.quantity = quantity;
